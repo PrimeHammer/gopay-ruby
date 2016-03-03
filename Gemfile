e6cb8f3df3df6d6f2b73edf7c40365a75b4cb02f
David Hrachovy <pair+david@npm.com>
2016-03-03 06:58:31 -0800

    prepare for initial release

--- Gemfile
+++ Gemfile
@@ -1,4 +1,12 @@
 source 'https://rubygems.org'
 
+gem 'rake'
+gem 'rspec'
+gem 'vcr'
+gem 'webmock'
+gem 'addressable'
+gem "coveralls"
+gem "simplecov"
+
 # Specify your gem's dependencies in gopay-ruby.gemspec
 gemspec
--- spec/gopay_spec.rb
+++ spec/gopay_spec.rb
@@ -36,18 +36,18 @@ describe GoPay::Payment, vcr: VCR_OPTIONS do
   end
 
   describe 'create' do
-    let(:params) { { payer: OpenStruct.new(allowed_methods: ["BANK_ACCOUNT"],
-                                           first_name: 'John',
-                                           last_name: 'Doe',
-                                           email: 'john@example.com'),
+    let(:params) { { payer: { allowed_payment_instruments: ["BANK_ACCOUNT"],
+                              contact: { first_name: 'John',
+                                          last_name: 'Doe',
+                                          email: 'john@example.com' } },
                      amount: 10000,
                      currency: 'CZK',
                      order_number: 'order-1',
                      order_description: 'foo',
                      lang: 'CS',
-                     callback: OpenStruct.new(return_url: 'http://localhost',
-                                              notification_url: 'http://localhost/2') } }
-    let(:recurrence_params) { { recurrence: OpenStruct.new(cycle: 'WEEK', period: 10, date_to: '2018-01-01') } }
+                     callback: { return_url: 'http://localhost',
+                                              notification_url: 'http://localhost/2' } } }
+    let(:recurrence_params) { { recurrence: { recurrence_cycle: 'WEEK', recurrence_period: 10, recurrence_date_to: '2018-01-01' } } }
 
     it 'returns existing payment for standard payment' do
       expect(GoPay::Payment.create(params)).to include(
