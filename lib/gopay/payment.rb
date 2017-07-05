f2fea13524234c623d01e8ed2e4900328cfcbf8d
Josef Hovad <josef.hovad@nobord.com>
2017-07-05 17:41:57 +0200

    added payment_instruments request

--- lib/gopay/payment.rb
+++ lib/gopay/payment.rb
@@ -3,6 +3,9 @@ require 'gopay/error'
 
 module GoPay
   class Payment
+    
+    # basic methods
+    
     def self.create(payment_data)
       target = { target: { type: "ACCOUNT", goid: GoPay.goid } }
       GoPay.request :post, "/api/payments/payment", body_parameters: payment_data.merge(target)
@@ -23,6 +26,14 @@ module GoPay
     def self.create_recurrence(original_payment_id, recurring_payment_data)
       GoPay.request :post, "/api/payments/payment/#{original_payment_id}/create-recurrence", body_parameters: recurring_payment_data
     end
+
+    # other methods
+
+    def self.payment_instruments(currency)
+      goid = GoPay.goid
+      GoPay.request :get, "/api/eshops/eshop/#{goid}/payment-instruments/#{currency}"
+    end
+    
 
   end
 end
