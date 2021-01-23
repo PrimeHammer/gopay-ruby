bcf51a6f6cccf2d73a96087fbe9d9b34db9db616
David Hrachovy <david.hrachovy@gmail.com>
2021-01-23 20:32:48 +0100

    Update ruby version, release a new gem version (#11)
    * add ruby 2.7 to Travis, remove 2.4
    
    * Release 0.4.1 version, require ruby 2.5
    
    * remove alpha from version
    
    * Update build icon in Readme

--- lib/gopay/version.rb
+++ lib/gopay/version.rb
@@ -2,11 +2,11 @@ module GoPay
   class Version
     MAJOR = 0
     MINOR = 4
-    PATCH = 0
+    PATCH = 1
 
     class << self
       def to_s
-        [MAJOR, MINOR, PATCH].compact.join('.') + '.alpha'
+        [MAJOR, MINOR, PATCH].compact.join('.')
       end
     end
   end
